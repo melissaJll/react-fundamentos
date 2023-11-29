5bd06eef865c49651cc87fc81d5f4ef2338d5492
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-11-29 11:17:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,39 @@ function App() {
           <a href="">Home</a>
           <a href="">Produtos</a>
           <a href="">Contatos</a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
         </nav>
       </header>
 
       <main>
-        <h2>Teste</h2>
+        <section>
+          <h2>Conteúdo da aplicação</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+            iure facere vitae impedit. Sequi. Lorem ipsum dolor sit amet <br />
+            consectetur, adipisicing elit. Rerum ullam qui fuga sit neque? Dicta
+            repellendus fuga nihil consectetur, animi porro a molestias!
+          </p>
+          <article>
+            <h3>Artigo 1</h3>
+            <p> conteúdo do artigo 1...</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p> conteúdo do artigo 2...</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p> conteúdo do artigo 3...</p>
+          </article>
+        </section>
       </main>
+
+      <hr />
+      <footer>
+        <h2>Rodapé</h2>
+      </footer>
     </>
   );
 }
