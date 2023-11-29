c9221e648ab33abbcd468a443e38384808b2c342
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-11-29 11:11:01 -0300

    formatação com prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-
 function App() {
   /* Comentario no js puro*/
 
-  return <>
-    {/* Comentario no jsx */}
-    <header>
-      <h1>Ola React 😊</h1>
-    </header>
-    <main> 
-      <h2>Teste</h2>
-    </main>
-  </>
-  
+  return (
+    <>
+      {/* Comentario no jsx */}
+      <header>
+        <h1>Olá React 😊</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Contatos</a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </nav>
+      </header>
+
+      <main>
+        <h2>Teste</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
