93afec4274cea85e6cedee5c63339790b66e8499
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-11-29 10:59:32 -0300

    comentário no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 function App() {
-  return (
-  <>
+  /* Comentario no js puro*/
+
+  return <>
+    {/* Comentario no jsx */}
     <header>
       <h1>Ola React 😊</h1>
     </header>
     <main> 
       <h2>Teste</h2>
     </main>
-  </>)
+  </>
   
 }
 
