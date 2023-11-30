1e4691afb19962fb7f9ca1080bff44fa4c500496
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-11-30 09:36:03 -0300

    css inline

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,14 @@
+//{} significa js  e {{objeto}}
 function Rodape() {
   return (
-    <footer>
+    <footer
+      style={{
+        backgroundColor: "lightblue",
+        padding: "2%",
+        color: "yellowgreen",
+        border: "solid 4px #8ac4d7",
+      }}
+    >
       <hr />
       <h2>Rodapé</h2>
     </footer>
