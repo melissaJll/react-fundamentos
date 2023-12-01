7016062196ab518695d4003e1c4bb4ff295e6e5c
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-12-01 09:39:35 -0300

    styled-components no menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,29 @@
+import styled from "styled-components";
+
+const StyledMenu = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: white;
+    padding: 7px;
+  }
+  a:hover {
+    background-color: #cc75be;
+    color: #3b2d2d;
+  }
+`;
+
 function Menu() {
   return (
-    <nav>
+    <StyledMenu>
       <a href="">Home</a>
       <a href="">Produtos</a>
       <a href="">Contatos</a>
-    </nav>
+    </StyledMenu>
   );
 }
 
