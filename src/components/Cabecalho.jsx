caecfe24cc9414e35130501b1f4d469d2ab0d884
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-12-01 09:29:20 -0300

    usando styled component com extensão

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,18 +2,31 @@ import Menu from "./Menu";
 // Impportação da lib styled components
 import styled from "styled-components";
 
+//Especifica que será dentro do header``
 const StyledCabecalho = styled.header`
-  background-color: blue;
+  //header{
+  background-color: #f3b2bd;
   color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+
+  hr {
+    background-color: #7a4ca5;
+    width: 50%;
+    border: none;
+    height: 4px;
+    margin: 8px auto;
+  }
 `;
 
 function Cabecalho() {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Olá React 😊</h1>
-      <Menu />
       <hr />
-    </header>
+      <Menu />
+    </StyledCabecalho>
   );
 }
 
