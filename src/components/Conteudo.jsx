42c167188557f95b5e3cc548e9e1a95a32eac6fc
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-14 16:41:17 -0300

    Evento click nos botões

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,12 +1,45 @@
 import styled from "styled-components";
 import Artigo from "./Artigo";
 import cursos from "../api/cursos";
-// console.log(cursos); teste
 
-// Importando os assets de imagem
-import imagem1 from "../assets/abra-o-livro.png";
-import imagem2 from "../assets/livro-magico.png";
-import imagem3 from "../assets/pilha-de-livros.png";
+function Conteudo() {
+  const aplicarFiltro = (event) => {
+    console.log(event.currentTarget.innerText);
+  };
+  return (
+    <StyledConteudo>
+      <h2 className="centralizar">Conteúdo da aplicação</h2>
+
+      <p className="centralizar">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        labore? Officia quae quo a quas excepturi distinctio sint voluptas
+        labore iste veniam possimus facere adipisci sit repellat, voluptate,
+        expedita aspernatur.
+      </p>
+
+      <div className="filtros">
+        <p>
+          <b>Filtrar por: </b>
+          <button onClick={aplicarFiltro}>Front-End</button>
+          <button onClick={aplicarFiltro}>Back-End</button>
+          <button onClick={aplicarFiltro}>Design</button>
+        </p>
+      </div>
+
+      <div className="artigos">
+        {cursos.map((curso) => (
+          <Artigo
+            key={curso.id}
+            categoria={curso.categoria}
+            titulo={curso.titulo}
+            preco={curso.preco}
+          />
+        ))}
+      </div>
+      
+    </StyledConteudo>
+  );
+}
 
 const StyledConteudo = styled.main`
   width: 83vw;
@@ -22,43 +55,25 @@ const StyledConteudo = styled.main`
   .centralizar {
     text-align: center;
   }
-  .artigos {
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
+  .filtros {
+    margin: 1rem 0;
+    padding: 1rem 0;
+    border-top: solid 2px;
+    border-bottom: solid 2px;
+  }
 
-    @media (max-width: 650px) {
-      flex-direction: column;
+    @media screen and (min-width: 650px) {
+    .artigos {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+
+      & article {
+        width: 48%;
+        margin: 1%;
+      }
     }
   }
 `;
 
-function Conteudo() {
-  return (
-    <StyledConteudo>
-      <section>
-        <h2 className="centralizar">Conteúdo da aplicação</h2>
-
-        <p className="centralizar">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
-          iure facere vitae impedit. Sequi. Lorem ipsum dolor sit amet <br />
-          consectetur, adipisicing elit. Rerum ullam qui fuga sit neque? Dicta
-          repellendus fuga nihil consectetur, animi porro a molestias!
-        </p>
-
-        <div className="artigos">
-          {cursos.map((curso) => (
-            // e return um artigo
-            <Artigo
-              key={curso.id}
-              titulo={curso.titulo}
-              categoria={curso.categoria}
-              preco={curso.preco}
-            ></Artigo>
-          ))}
-        </div>
-      </section>
-    </StyledConteudo>
-  );
-}
 export default Conteudo;
