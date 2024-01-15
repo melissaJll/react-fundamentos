ea4b78c092c7410a3816ce5a2344ea0270d150b9
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-15 11:24:09 -0300

    useEffect

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Produtos() {
   // o state "produtos" é iniciado como um array vazio. Posteriormente (após o carregamento de dados da API). ele será preenchidp cos os objetos/produtos.
 
   const [produtos, setProdutos] = useState([]);
 
-  const carregarDados = async () => {
-    try {
-      const resposta = await fetch("https://fakestoreapi.com/products");
-      const dados = await resposta.json();
-      console.log(dados);
-      // setProdutos(dados);
-    } catch (error) {
-      console.error("Houve um erro: " + error);
-    }
-  };
+  // Gerenciando o efeito colateral do carregamento dos dados da API
+  // o primeiro parametro é uma função Callback
+  useEffect(() => {
+    // carrega uma vez como efeito da renderização da página
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch("https://fakestoreapi.com/products");
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.error("Houve um erro: " + error);
+      }
+    };
 
-  carregarDados();
+    carregarDados();
+  }, []);
+  //, segundo parametro lista
 
   return <h2>Produtos</h2>;
 }
