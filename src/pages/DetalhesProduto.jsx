5c758d1ba9a35055baf1f57e4a580427a9755b07
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-16 10:27:07 -0300

    loading

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import imageloading from "../assets/loading.svg";
 
 function DetalhesProduto() {
   // Usamos o hook useParams do react router DOM para ter acesso aos parametros da rota dinâmica neste caso, o parametro chamdado "id".
@@ -8,6 +9,8 @@ function DetalhesProduto() {
   const [produto, setProduto] = useState({});
   //   {} - Objeto trazido de apenas um produto
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const carregarDados = async () => {
       try {
@@ -15,6 +18,7 @@ function DetalhesProduto() {
         const dados = await resposta.json();
         console.log(dados);
         setProduto(dados);
+        setLoading(false);
       } catch (error) {
         console.error("Erro ao carregar produto: " + error);
       }
@@ -25,17 +29,25 @@ function DetalhesProduto() {
 
   return (
     <article>
-      <h2>{produto.title} </h2>
-      <p>{produto.description}</p>
-      <p>
-        <b>Categoria: {produto.category}</b>
-      </p>
+      {loading ? (
+        <p style={{ textAlign: "center" }}>
+          <img src={imageloading} alt="Carregando" />
+        </p>
+      ) : (
+        <section>
+          <h2>{produto.title} </h2>
+          <p>{produto.description}</p>
+          <p>
+            <b>Categoria: {produto.category}</b>
+          </p>
 
-      <p>
-        <b>Preço: {produto.price}</b>
-      </p>
+          <p>
+            <b>Preço: {produto.price}</b>
+          </p>
 
-      <img src={produto.image} alt="" height={500} />
+          <img src={produto.image} alt="" height={500} />
+        </section>
+      )}
     </article>
   );
 }
