df5332f1bf7928ac33250c2951629eab1797d2b2
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-16 09:41:18 -0300

    usando state para capturar dados e definir com var global produto

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function DetalhesProduto() {
   // Usamos o hook useParams do react router DOM para ter acesso aos parametros da rota dinâmica neste caso, o parametro chamdado "id".
   const { id } = useParams();
+
+  const [produto, setProduto] = useState([]);
 
   useEffect(() => {
     const carregarDados = async () => {
@@ -11,6 +13,7 @@ function DetalhesProduto() {
         const resposta = await fetch(`https://fakestoreapi.com/products/${id}`);
         const dados = await resposta.json();
         console.log(dados);
+        setProduto(dados);
       } catch (error) {
         console.error("Erro ao carregar produto: " + error);
       }
@@ -21,14 +24,14 @@ function DetalhesProduto() {
 
   return (
     <article>
-      <h2>Titulo </h2>
-      <p>Descrição </p>
+      <h2>{produto.title} </h2>
+      <p>{produto.description}</p>
       <p>
-        <b>Categoria:</b>
+        <b>Categoria: {produto.category}</b>
       </p>
 
       <p>
-        <b>Preço:</b>
+        <b>Preço: {produto.price}</b>
       </p>
 
       <img src="" alt="" />
